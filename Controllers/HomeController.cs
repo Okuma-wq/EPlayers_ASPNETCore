e938dffa4670617b4df4f175a68f66c3a9c263be
pedro Okuma <pedrookuma3@gmail.com>
2021-01-19 17:59:52 -0300

    Login Implementado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EPlayers_ASPNETCore.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace EPlayers_ASPNETCore.Controllers
 {
@@ -20,6 +21,7 @@ namespace EPlayers_ASPNETCore.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.UserName = HttpContext.Session.GetString("_UserName");
             return View();
         }
 
